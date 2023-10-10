c94e30c116e56345741ab9b3f458020dc81835a3
Ricardo <ric.dalvarez@hotmail.com>
2023-10-10 16:04:17 +0200

    add update order and create new orderDonation

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -4,6 +4,7 @@
 
 import { factories } from "@strapi/strapi";
 import { isValidPostalCode } from "../services/coverageService";
+import { sendConfirmationEmail } from "../services/confirmationEmail";
 
 export default factories.createCoreController(
   "api::order.order",
@@ -14,17 +15,52 @@ export default factories.createCoreController(
 
         const authenticatedUser = ctx.state.user;
         const { order_meta } = ctx.request.body;
-        const order = await strapi
-          .service("api::order.order")
-          .findOne(sanitizedQueryParams, {
-            populate: ["order_items", "order_meta"],
-          });
+        const orderId = ctx.request.params.id;
+
+        const orderService = strapi.service("api::order.order");
 
         /***** Rest of the code here *****/
+
+        const order = await orderService.findOne(orderId, {
+          populate: ["order_items", "order_meta"],
+        });
+
+        order.status = "cancelled";
+
+        //find pedido by id and update status to cancelled,
+
+        const updatedOrder = await orderService.update(orderId, {
+          data: {
+            status: "cancelled",
+          },
+        });
+
+        //create new pedido with status processing, type donation
+
+        const newOrderDonation = await orderService.create({
+          data: {
+            status: "processing",
+            type: "donation",
+          },
+        });
+        console.log(newOrderDonation);
+
+        // const orders = await orderService.find({});
+        // console.log("orders", orders);
 
         if (!isValidPostalCode(order_meta.shipping_postcode)) {
           return "Código postal inválido";
         }
+
+        //create new pedido with status processing, type donation
+
+        //create new orderMeta with data from request body  and link with new order
+
+        //create new orderItem link with new Order
+
+        //send confirmation email to user
+
+        sendConfirmationEmail(order_meta.shipping_firstname);
 
         return {
           order,
