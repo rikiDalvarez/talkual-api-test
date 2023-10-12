8149b83b4a5f90840138a4a5a4baacdd6b7e159e
Ricardo <ric.dalvarez@hotmail.com>
2023-10-12 22:41:38 +0200

    fix awaiting service

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -14,12 +14,9 @@ export default factories.createCoreController(
   ({ strapi }) => ({
     async donate(ctx): Promise<any> {
       try {
-        const sanitizedQueryParams = await this.sanitizeQuery(ctx);
-
         const { order_meta } = (ctx.request as any).body;
         const orderId = (ctx.request as any).params.id;
 
-        const orderService = strapi.service("api::order.order");
         //TODO Add order interface
         //validation
         if (!Number.isInteger(Number(orderId))) {
@@ -27,37 +24,14 @@ export default factories.createCoreController(
           throw error;
         }
 
-        const order = await orderService.findOne(orderId, {
-          populate: ["order_items", "order_meta"],
-        });
-
-        if (!order) {
-          const error = new ErrorHandler("bad_orderId");
-          throw error;
-        }
-
-        if (order.status === "cancelled") {
-          const error = new ErrorHandler("order_cancelled");
-          throw error;
-        }
-
-        let test = donateOrder(ctx);
-        console.log("test", test);
-
         if (!isValidPostalCode(order_meta.shipping_postcode)) {
           const error = new ErrorHandler("bad_order_meta");
           throw error;
         }
-
+        const result = await donateOrder(ctx);
         sendConfirmationEmail(order_meta.shipping_firstname);
-        return test;
-        // return {
-        //   order: newOrderDonation,
-        //   order_meta: {
-        //     shipping_postcode: newOrderMeta.shipping_postcode,
-        //     shipping_firstname: newOrderMeta.shipping_firstname,
-        //   },
-        // };
+
+        return result;
       } catch (error) {
         switch (error.name) {
           case "bad_orderId":
--- src/api/order/services/donateOrder.ts
+++ src/api/order/services/donateOrder.ts
@@ -1,5 +1,6 @@
 //encontrar order id , status cancelled, y crear nuevo pedido con status processing, type donation
 // verificar order-items  con orderID
+import { ErrorHandler } from "../../../errorHandler";
 
 export async function donateOrder(ctx) {
   const orderItemQuery = strapi.query("api::order-item.order-item");
@@ -12,61 +13,75 @@ export async function donateOrder(ctx) {
   const orderId = (ctx.request as any).params.id;
 
   //TODO Add order interface
-  // const order = await orderService.findOne(orderId, {
-  //   populate: ["order_items", "order_meta"],
-  // });
+  try {
+    const order = await orderService.findOne(orderId, {
+      populate: ["order_items", "order_meta"],
+    });
 
-  const updatedOrder = await orderService.update(orderId, {
-    data: {
-      status: "cancelled",
-    },
-    populate: ["order_items", "order_meta"],
-  });
-
-  const newOrderDonation = await orderService.create({
-    data: {
-      status: "processing",
-      type: "donation",
-      user: authenticatedUser.id,
-    },
-  });
-
-  const newOrderMeta = await orderMetaService.create({
-    data: {
-      ...order_meta,
-      order: newOrderDonation.id,
-    },
-  });
-
-  const orderItemList = await orderItemQuery.findMany({
-    where: {
-      order: {
-        id: orderId,
-      },
-    },
-  });
-
-  const createBuilkOrderItems = async (list) => {
-    for (const orderItem of list) {
-      const newSku = Math.random().toString(36).substring(7);
-      const newOrderItem = await orderItemService.create({
-        data: {
-          quantity: orderItem.quantity,
-          sku: newSku,
-          order: newOrderDonation.id,
-          price: orderItem.price,
-        },
-      });
+    if (!order) {
+      const error = new ErrorHandler("bad_orderId");
+      throw error;
     }
-  };
 
-  await createBuilkOrderItems(orderItemList);
+    if (order.status === "cancelled") {
+      const error = new ErrorHandler("order_cancelled");
+      throw error;
+    }
 
-  return {
-    order: newOrderDonation,
-    order_meta: {
-      shipping_postcode: newOrderMeta.shipping_postcode,
-      shipping_firstname: newOrderMeta.shipping_firstname,
-    },
-  };
+    const updatedOrder = await orderService.update(orderId, {
+      data: {
+        status: "cancelled",
+      },
+      populate: ["order_items", "order_meta"],
+    });
+
+    const newOrderDonation = await orderService.create({
+      data: {
+        status: "processing",
+        type: "donation",
+        user: authenticatedUser.id,
+      },
+    });
+
+    const newOrderMeta = await orderMetaService.create({
+      data: {
+        ...order_meta,
+        order: newOrderDonation.id,
+      },
+    });
+
+    const orderItemList = await orderItemQuery.findMany({
+      where: {
+        order: {
+          id: orderId,
+        },
+      },
+    });
+
+    const createBuilkOrderItems = async (list) => {
+      for (const orderItem of list) {
+        const newSku = Math.random().toString(36).substring(7);
+        const newOrderItem = await orderItemService.create({
+          data: {
+            quantity: orderItem.quantity,
+            sku: newSku,
+            order: newOrderDonation.id,
+            price: orderItem.price,
+          },
+        });
+      }
+    };
+
+    await createBuilkOrderItems(orderItemList);
+
+    return {
+      order: newOrderDonation,
+      order_meta: {
+        shipping_postcode: newOrderMeta.shipping_postcode,
+        shipping_firstname: newOrderMeta.shipping_firstname,
+      },
+    };
+  } catch (error) {
+    throw error;
+  }
 }
